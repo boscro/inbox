3c8226d7fae7465b510bf1f8a8c65d4d9eac3506
Smith <cryptokeeper666@gmail.com>
2019-07-21 16:09:45 -0400

    crud

--- contracts/Crud.sol
+++ contracts/Crud.sol
@@ -6,27 +6,31 @@ contract Crud{
         string name;
     }
     User[] public users;
-    uint public nextId;
+    uint public nextId=1;
     
     function create(string memory name) public {
         users.push(User(nextId, name));
         nextId++;
     }
     function read(uint id) view public returns(uint,string memory){
-        for(uint i=0; i < users.length; i++){
-            if(users[i].id == id){
-                return (users[i].id, users[i].name);
-            }
-        }
+        uint i = find(id);
+        return (users[i].id, users[i].name);
+            
     }
     function update(uint id, string memory name )public{
-        for(uint i=0; i < users.length; i++){
-            if(users[i].id == id){
-                users[i].name=name;
-            }
-        }        
+        uint i = find(id);
+        users[i].name=name;
     }
     function destroy(uint id)public{
-        delete users[id];
+        uint i = find(id);
+        delete users[i];
+    }
+    function find(uint id) view internal returns(uint){
+        for(uint i=0; i < users.length; i++){
+            if(users[i].id == id){
+                return i;
+            }
+        }
+        revert('User does not exist!');
     }
 }
